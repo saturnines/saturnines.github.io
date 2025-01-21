953437d04d8c4a27e4bc177afab57e758ab319f9
Kevin <105906233+saturnines@users.noreply.github.com>
2025-01-21 08:48:16 -0600

    Update vite.config.js
    please work

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/privportweb/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
 })
