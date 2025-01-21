6f3ed4f6f931db67dfc991e7374554c31300db5a
Kevin <105906233+saturnines@users.noreply.github.com>
2025-01-21 07:42:15 -0600

    Update App.jsx
    workflows fix i think

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Resume from './pages/Resume';
