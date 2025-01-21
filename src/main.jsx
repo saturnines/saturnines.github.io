f9129397fc6455544509bc33d0f1e6861bfbefa3
Kevin <105906233+saturnines@users.noreply.github.com>
2025-01-21 09:21:34 -0600

    Update main.jsx
    changed to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client"
 import App from "./app.jsx"  
 import "./index.css"
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
