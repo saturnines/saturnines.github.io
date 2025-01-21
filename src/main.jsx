ce957aaefa6d2a703e971c0401a2783ea6f44048
Kevin <105906233+saturnines@users.noreply.github.com>
2025-01-21 09:23:22 -0600

    Update main.jsx
    added import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./app.jsx"  
+import { HashRouter } from "react-router-dom"
+import App from "./app.jsx"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
