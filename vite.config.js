dd8c3744b6c82b8993d16ac2d620ad061aee07ac
Kevin <105906233+saturnines@users.noreply.github.com>
2025-01-21 08:53:14 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-mport { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 
 export default defineConfig({
   plugins: [react()],
   base: '/privportweb/',
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
 })
