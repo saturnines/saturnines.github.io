d889d336a3d4610d5262596dad9f652eabc13cd4
Kevin <105906233+saturnines@users.noreply.github.com>
2025-01-22 20:04:42 -0600

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Home from './pages/home';
 import Resume from './pages/resume';
 import Projects from './pages/projects';
 
 export default function App() {
-  return (
-    <BrowserRouter basename="/privportweb">
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+ return (
+   <HashRouter>
+     <div className="min-h-screen bg-gray-50">
+       <Navbar />
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/resume" element={<Resume />} />
+         <Route path="/projects" element={<Projects />} />
+       </Routes>
+     </div>
+   </HashRouter>
+ );
 }
